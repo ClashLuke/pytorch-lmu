8f17be366377e34faa245bd55b9d33e752b93148
Lucas Nestler <39779310+ClashLuke@users.noreply.github.com>
2021-10-31 01:09:30 +0200

    perf: conv > linear

--- lmu.py
+++ lmu.py
@@ -19,8 +19,8 @@ class LMUFFT(nn.Module):
         self.seq_len = seq_len
         self.theta = theta
 
-        self.W_u = nn.Linear(in_features=input_size, out_features=fft_features + hidden_size)
-        self.W_h = nn.Linear(in_features=memory_size * fft_features, out_features=hidden_size)
+        self.W_u = nn.Conv1d(input_size, fft_features + hidden_size, (1,))
+        self.W_h = nn.Conv1d(memory_size * fft_features, hidden_size, (1,))
 
         Q = np.arange(self.memory_size, dtype=np.float64).reshape(-1, 1)
         R = (2*Q + 1) / self.theta
@@ -55,15 +55,13 @@ class LMUFFT(nn.Module):
         self.register_buffer("fft_H", fft_H.unsqueeze(0))  # [1, memory_size, seq_len + 1]
 
     def forward(self, x):
-        batch_size, seq_len, input_size = x.shape
+        batch_size, input_size, seq_len = x.shape
         out = self.W_u(x)
-        out0 = out[:, :, :self.hidden_size]
-        u = torch.relu(out[:, :, self.hidden_size:) # [batch_size, seq_len, 1]
-        fft_input = u.transpose(1, 2) # [batch_size, 1, seq_len]
+        out0 = out[:, :self.hidden_size]
+        u = torch.relu(out[:, self.hidden_size:) # [batch_size, seq_len, 1]
         fft_u = fft.rfft(fft_input, n=2 * seq_len, dim=-1) # [batch_size, seq_len, seq_len+1]
         temp = fft_u.view(-1, 1, seq_len) * self.fft_H  # [batch_size, memory_size, seq_len+1]
         m = fft.irfft(temp, n=2 * seq_len, dim=-1) # [batch_size, memory_size, seq_len+1]
         m = m[:, :, :-1] # [batch_size, memory_size, seq_len]
         m = m.view(batch_size, -1, seq_len)
-        m = m.transpose(1, 2) # [batch_size, seq_len, memory_size]
-        return torch.relu(self.W_h(m) + out0) # [batch_size, seq_len, hidden_size]
+        return self.W_h(m) + out0  # [batch_size, seq_len, hidden_size]
