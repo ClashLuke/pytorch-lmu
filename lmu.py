c3d632cef0ed94357bd2d36883204e6b201aa3ce
Lucas Nestler <39779310+ClashLuke@users.noreply.github.com>
2021-10-31 00:57:25 +0200

    Update lmu.py

--- lmu.py
+++ lmu.py
@@ -10,22 +10,6 @@ from scipy.signal import cont2discrete
 
 
 class LMUFFT(nn.Module):
-    """
-    Parallelized LMU Layer
-
-    Parameters:
-        input_size (int) : 
-            Size of the input vector (x_t)
-        hidden_size (int) : 
-            Size of the hidden vector (h_t)
-        memory_size (int) :
-            Size of the memory vector (m_t)
-        seq_len (int) :
-            Size of the sequence length (n)
-        theta (int) :
-            The number of timesteps in the sliding window that is represented using the LTI system
-    """
-
     def __init__(self, input_size, hidden_size, memory_size, seq_len, theta):
 
         super(LMUFFT, self).__init__()
@@ -71,29 +55,13 @@ class LMUFFT(nn.Module):
         self.register_buffer("fft_H", fft_H.unsqueeze(0))  # [1, memory_size, seq_len + 1]
 
     def forward(self, x):
-        """
-        Parameters:
-            x (torch.tensor): 
-                Input of size [batch_size, seq_len, input_size]
-        """
-
         batch_size, seq_len, input_size = x.shape
-
-        # Equation 18 of the paper
         u = torch.relu(self.W_u(x)) # [batch_size, seq_len, 1]
-
-        # Equation 26 of the paper
         fft_input = u.transpose(1, 2) # [batch_size, 1, seq_len]
         fft_u = fft.rfft(fft_input, n=2 * seq_len, dim=-1) # [batch_size, seq_len, seq_len+1]
-
-        # Element-wise multiplication (uses broadcasting)
-        # [batch_size, 1, seq_len+1] * [1, memory_size, seq_len+1]
         temp = fft_u * self.fft_H  # [batch_size, memory_size, seq_len+1]
-
         m = fft.irfft(temp, n=2 * seq_len, dim=-1) # [batch_size, memory_size, seq_len+1]
         m = m[:, :, :-1] # [batch_size, memory_size, seq_len]
         m = m.transpose(1, 2) # [batch_size, seq_len, memory_size]
-
-        # Equation 20 of the paper (W_m@m + W_x@x  W@[m;x])
         input_h = torch.cat((m, x), dim=-1) # [batch_size, seq_len, memory_size + input_size]
         return torch.relu(self.W_h(input_h)) # [batch_size, seq_len, hidden_size]
